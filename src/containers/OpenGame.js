0a8a86ab4c043e3e9c679e8c3a8f57897996bee6
kiselev <kiselev@factory.mn>
2016-10-08 23:13:18 +0300

    fix merge

--- src/containers/OpenGame.js
+++ src/containers/OpenGame.js
@@ -34,12 +34,10 @@ export default class OpenGame extends Component {
           <Title>Описание</Title>
         </Header>
 
-<<<<<<< 718f3dbd687972433d7167c1cf47c4bb48c1ceb6
         <Grid>
           <Col style={{ backgroundColor: '#D954D7', height: 200 }}>1</Col>
           <Col style={{ backgroundColor: '#D93735', height: 200  }}>2</Col>
         </Grid>
-=======
         <Content>
           <Grid>
             <Row>
@@ -63,7 +61,6 @@ export default class OpenGame extends Component {
             </Row>
           </Grid>
         </Content>
->>>>>>> upd OpenGame scene
       </Container>
     );
   }
